a2462d56d8261e139037434dceab5f51c2658f02
Ankit Ojha <ankitojha2004@gmail.com>
2026-09-05 22:36:47 +0530

    fix error

--- backend.py
+++ backend.py
@@ -34,8 +34,8 @@ graph = StateGraph(ChatState)
 
 graph.add_node('chat node',chat_node)
 
-graph.add_edge(START,chat_node)
-graph.add_edge(chat_node,END)
+graph.add_edge(START,"chat node")
+graph.add_edge("chat node",END)
 
 chatbot = graph.compile()
 
