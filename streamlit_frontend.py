db8b0ead7533a718914a759ff1f07e99b97dcb6a
Ankit Ojha <ankitojha2004@gmail.com>
2026-09-06 19:15:14 +0530

    create basic frontend using streamlit

---
+++ streamlit_frontend.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from backend import chatbot
+# st.session_state -> dict ->
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here ')
+
+if user_input:
+
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    st.session_state['message_history'].append({'role':'user','content':user_input})
+    with st.chat_message('assistant'):
+        st.text(chatbot.invoke(user_input))
