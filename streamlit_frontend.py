00f8a4ca7b163387fa32addd4c7c7011c8035e88
Ankit Ojha <ankitojha2004@gmail.com>
2026-09-07 23:16:03 +0530

    make the difference in user and assitant

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -24,6 +24,6 @@ if user_input:
     response = chatbot.invoke({'messages':[HumanMessage(content = user_input)]},config=CONFIG)
     ai_message = response['messages'][-1].content
 
-    st.session_state['message_history'].append({'role':'user','content':ai_message})
+    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
     with st.chat_message('assistant'):
         st.text(ai_message)
